899940fe5dde7b56156e480f3289ed38d3912a3b
Märt Tender <mart.tender@ut.ee>
2021-01-27 15:14:05 +0200

    v1.1

--- regaja.py
+++ regaja.py
@@ -4,21 +4,25 @@ import time
 
 
 def register(username, password):
-    driver = webdriver.Firefox()
-
     driver.get('https://ois2.ut.ee/')
+    time.sleep(5)
 
-    time.sleep(1)
-    driver.find_element_by_xpath("//button[@color='primary']").click()
-    driver.find_element_by_id("username").send_keys(username)
-    driver.find_element_by_id("password").send_keys(password)
-    driver.find_element_by_xpath("//button[@type='submit']").click()
-    time.sleep(2)
-    driver.find_element_by_link_text('Registreeringud').click()
-    time.sleep(1)
+    try:
+        driver.find_element_by_link_text('Registreeringud').click()
+    except NoSuchElementException:
+        driver.find_element_by_xpath("//button[@color='primary']").click()
+        time.sleep(2)
+        driver.find_element_by_id("username").send_keys(username)
+        driver.find_element_by_id("password").send_keys(password)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        time.sleep(10)
+        driver.find_element_by_link_text('Registreeringud').click()
+
+    time.sleep(5)
     while True:
         try:
             driver.find_element_by_id('mat-checkbox-1').click()
+            time.sleep(5)
             target = driver.find_elements_by_xpath("//button[@class='full-width mat-raised-button "
                                                    "mat-button-base mat-primary ng-star-inserted']")
             target[1].click()
@@ -30,8 +34,14 @@ def register(username, password):
             time.sleep(10)
 
 
-# Muuda ainult järgmist kahte rida
+# Muuda ainult j2rgmist kahte rida
 usr = "Sinu Õis2 kasutajanimi"
 pwd = "Sinu Õis2 parool"
 
-register(usr, pwd)
+driver = webdriver.Firefox()
+while True:
+    try:
+        register(usr, pwd)
+        break
+    except Exception:
+        time.sleep(10)
