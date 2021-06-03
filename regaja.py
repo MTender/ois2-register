0df4c99c4bde5299b78fa8375ed33e8c1f6e883a
Märt Tender <mart.tender@ut.ee>
2021-06-03 09:57:13 +0300

    Code cleanup

--- regaja.py
+++ regaja.py
@@ -4,34 +4,35 @@ import time
 
 
 def register(username, password):
-    driver.get('https://ois2.ut.ee/')
-    time.sleep(5)
+	driver.get('https://ois2.ut.ee/')
+	time.sleep(5)
 
-    try:
-        driver.find_element_by_xpath("// div[contains(text(), 'Registreeringud')]").click()
-    except NoSuchElementException:
-        driver.find_element_by_xpath("//button[@color='primary']").click()
-        time.sleep(2)
-        driver.find_element_by_id("username").send_keys(username)
-        driver.find_element_by_id("password").send_keys(password)
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-        time.sleep(10)
-        driver.find_element_by_xpath("// div[contains(text(), 'Registreeringud')]").click()
+	try:
+		driver.find_element_by_xpath("//div[contains(text(), 'Registreeringud')]").click()
+	except NoSuchElementException:
+		driver.find_element_by_xpath("//button[@color='primary']").click()
+		time.sleep(2)
+		driver.find_element_by_id("username").send_keys(username)
+		driver.find_element_by_id("password").send_keys(password)
+		driver.find_element_by_xpath("//button[@type='submit']").click()
+		time.sleep(10)
+		driver.find_element_by_xpath("// div[contains(text(), 'Registreeringud')]").click()
 
-    time.sleep(5)
-    while True:
-        try:
-            driver.find_element_by_id('mat-checkbox-1').click()
-            time.sleep(5)
-            target = driver.find_elements_by_xpath("//button[@class='mat-focus-indicator full-width mat-raised-button mat-button-base mat-primary mat-button-disabled ng-star-inserted']")
-            target[1].click()
-        except IndexError:
-            print("Registreeritud!")
-            break
-        except NoSuchElementException:
-            driver.refresh()
-        driver.refresh()
-        time.sleep(10)
+	time.sleep(5)
+	while True:
+		try:
+			driver.find_element_by_id('mat-checkbox-1').click()
+			time.sleep(5)
+			target = driver.find_elements_by_xpath(
+				"//button[@class='mat-focus-indicator full-width mat-raised-button mat-button-base mat-primary mat-button-disabled ng-star-inserted']")
+			target[1].click()
+		except IndexError:
+			print("Registreeritud!")
+			break
+		except NoSuchElementException:
+			driver.refresh()
+		driver.refresh()
+		time.sleep(10)
 
 
 # Muuda ainult j2rgmist kahte rida
@@ -40,8 +41,8 @@ pwd = "Sinu Õis2 parool"
 
 driver = webdriver.Firefox()
 while True:
-    try:
-        register(usr, pwd)
-        break
-    except Exception:
-        time.sleep(10)
+	try:
+		register(usr, pwd)
+		break
+	except Exception:
+		time.sleep(10)
